886fbeebfe5d67eecf2c68cd9886ba8ab1b966bd
Ted Lee <Ted@TedLee.me>
2013-01-30 15:30:51 -0500

    Implemented link adding on a users profile page. The users links (currently all of them) are listed on their profile page

--- app.rb
+++ app.rb
@@ -15,6 +15,8 @@ class User
 	# For some reason adding the property id leads to the following error: 
 	# The number of arguments for the key is invalid, expected 2 but was 1
 
+	# This might because of the number of arguments that are passed in at POST /:signup
+
 	#property :id, 			Serial
 	property :username,		String, required: true, :key => true
 	property :firstname, 	String, required: true
@@ -34,6 +36,7 @@ class Link
 	include DataMapper::Resource
 
 	property :id,			Serial
+	property :url,			String, :length => 200
 	property :title,		String, :length => 120
 	property :description,	String, :length => 120
 
@@ -43,16 +46,18 @@ end
 
 configure :development do
 	DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/shortlist")
-	#DataMapper.finalize
-	DataMapper.auto_migrate! # wipes everything
+	DataMapper.auto_upgrade!
+	DataMapper.finalize
+	#DataMapper.auto_migrate! # wipes everything
 end 
 
 configure :production do
 	DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/shortlist")
+	DataMapper.auto_upgrade!
 	DataMapper.finalize
 
 	#shouldn't run auto upgrade in production
-	DataMapper.auto_upgrade!
+	
 end
 
 get "/" do
@@ -74,14 +79,33 @@ get "/changelog" do
 end
 
 post "/signup" do
-  User.create(:username => params[:username], :firstname => params[:firstname], :lastname => params[:lastname], :user_avatar => params[:user_avatar], :created_at => Time.now)
-  redirect back
+	User.create(:username => params[:username], :firstname => params[:firstname], :lastname => params[:lastname], :user_avatar => params[:user_avatar], :created_at => Time.now)
+	redirect back
+end
+
+post "/:username/add" do
+	@user = User.get params[:username]
+
+	puts "username submitted is: #{@user.username}"
+
+	if @user
+
+		Link.create(:url => params[:url], :title => params[:title], :description => params[:description],  :user_username => @user.username)
+		redirect back
+	end
 end
 
 get "/:username" do
 	@user = User.get params[:username]
+
+	if @user.links
+		@links = Link.all(:user_username => @user.username)
+	end
+
+	# To get all the links from a user do @user.links.all()
+
 	if @user
-		@username = "#{params[:username]}"
+		@title = "The Shortlist of #{@user}"
 		erb :user
 	else
 		"That user doesn't exist yet :("
